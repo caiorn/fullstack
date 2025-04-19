a7ebef4b27313785715c30c17b3b13189ddf5e39
caiorn <caio.ssn96@gmail.com>
2025-04-19 13:26:59 -0300

    init backend with fastify

--- frontend/src/loaders/EquipamentsLoader.js
+++ frontend/src/loaders/EquipamentsLoader.js
@@ -1,3 +1,13 @@
+/**
+ * Loader functions são usadas pelo React Router para buscar dados
+ * antes da renderização de uma rota. Elas permitem carregar e preparar
+ * informações necessárias para o componente, melhorando a experiência do usuário
+ * ao evitar carregamentos visuais (como "piscar" de conteúdo).
+ *
+ * As funções loader recebem um objeto com `params`, `request`, entre outros,
+ * e devem retornar os dados que serão acessados com `useLoaderData()` no componente.
+ */
+
 export default function loadEquipament({params}) {   
     console.log("string param:" + params.equipamentId);
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
+import {useEffect, useState} from 'react';
 import { Outlet, Link } from 'react-router-dom';
 import styles from './Home.module.css';
 import PrimaryButton from '../../components/Button/PrimaryButton'
 import SecondaryButton from '../../components/Button/SecondaryButton'
 
-export default function Home() {
+
+export default function Home() {  
+  
+  const [strList, setList] = useState([])
+
+  useEffect(() => {
+    console.log('strList', strList);
+  }, [strList])    // executará na 1º renderizacao e em cada atualizacao do estado strList
+
+  const addItem = (item) => {
+    setList(prev => [...prev, item])
+  }
+
   return (
     <div className={styles.Home}>
       <h1>Bem-vindo à Página Inicial</h1>
@@ -13,7 +25,13 @@ export default function Home() {
       <time title="12 de Abril às 11:47h" dateTime='2025-04-12 11:4600'>Publicado há 10min</time>
       {/* Renderiza as subrotas aqui */}
       <h1></h1>
- 
+       
+      <button onClick={() => addItem('Novo Item')}>Adicionar Item</button>
+      <ul>
+        {strList.map((item, index) => (
+          <li key={index}>{item} {index}</li>
+        ))}  
+      </ul> 
       <Outlet />
     </div>
   );
